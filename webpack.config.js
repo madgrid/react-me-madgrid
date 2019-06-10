ef98b7931c2229ba52e27668d1261a9f8d0de95d
madgrid <aaronmadved@gmail.com>
2019-06-10 10:12:47 +0200

    added webpack to handle js and babel, images, html, and css

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                // : /node_modules/,
+                loader: ['style-loader', 'css-loader'],
+            },
+            {
+              test: /\.(png|jp(e*)g|svg)$/,
+              exclude: /node_modules/, 
+              use: [{
+                  loader: 'url-loader',
+                  options: { 
+                      limit: 8000, // Convert images < 8kb to base64 strings
+                      name: 'images/[hash]-[name].[ext]'
+                  } 
+              }]
+          }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './public/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000'
+        })
+    }
+}
